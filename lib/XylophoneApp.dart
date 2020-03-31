41819b77be3cef02f5685cec18361f7ee5aaa4d8
hannah <huilee9999@gmail.com>
2020-03-31 08:29:10 -0400

    add audio player function

---
+++ lib/XylophoneApp.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+
+  void playSound(int index){
+    final player = AudioCache();
+    player.play('note$index.wav');
+  }
+
+  Widget buildKey({Color color, int soundNum}){
+   return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNum);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.amber, soundNum: 1),
+              buildKey(color: Colors.red, soundNum: 2),
+              buildKey(color: Colors.green, soundNum: 3),
+              buildKey(color: Colors.pink, soundNum: 4),
+              buildKey(color: Colors.teal, soundNum: 5),
+              buildKey(color: Colors.deepOrange, soundNum: 6),
+              buildKey(color: Colors.indigo, soundNum: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
